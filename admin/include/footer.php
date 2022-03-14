3f66b94b506439777a20e3f3b146a78671ae695a
pranavprakashgithub <pranavprakash4777@gmail.com>
2022-03-14 20:01:12 +0530

    Full frontend Designed

---
+++ admin/include/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+		<div class="container">
+
+
+			<b class="copyright">&copy; Team Hashtag </b> All rights reserved.
+		</div>
+	</div>
+	<style>.footer{background-color: black; color: white;} .copyright{color: white;}</style>
